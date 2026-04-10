fa2bce8a84d4137a03c401466514b97b21285d74
Krishav Raj Singh <krishavrajsingh@gmail.com>
2026-04-10 20:22:52 +0530

    vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
+import { Analytics } from "@vercel/analytics/next"
 import type { AppProps } from "next/app"
 
 import "~styles/tailwind.css"
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default App
